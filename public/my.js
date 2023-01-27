a7f36013dcdd5a338c36bfaf3809957c6af06208
krisztian <molnarkrisztian@ugmszi.hu>
2023-01-27 12:08:32 +0100

    javítás

--- public/my.js
+++ public/my.js
@@ -1,1 +1,29 @@
 console.log("megettem");
+const url = "http://localhost:3000/products/"
+
+async function getProducts() {
+    let response = await fetch(url)
+    let data = await response.json();
+
+    console.log(data);
+}
+getProducts();
+
+async function postProduct() {
+    let body = 
+    {
+        name: "Á79",
+        quantity: 699,
+        price: 6500,
+        type: "fémáru"
+    }
+    let response = await fetch(url, {
+        Method: 'POST',
+        Headers: {
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+        },
+        Body: body,
+        Cache: 'default'
+    })
+}
